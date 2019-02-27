253ac8c6b840f1413763d3004aca1f38c9d279ee
Cory Lee <cory@edx.org>
2019-02-27 17:09:26 -0500

    Update acceptance.py

--- lms/envs/acceptance.py
+++ lms/envs/acceptance.py
@@ -147,7 +147,6 @@ FEATURES['ENABLE_FEEDBACK_SUBMISSION'] = False
 
 # Include the lettuce app for acceptance testing, including the 'harvest' django-admin command
 INSTALLED_APPS.append('lettuce.django')
-
 LETTUCE_APPS = ('courseware', 'instructor')
 
 # Lettuce appears to have a bug that causes it to search
