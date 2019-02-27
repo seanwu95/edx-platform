db094f7b7871d54e405f9a19716577d8c79c402d
Cory Lee <cory@edx.org>
2019-02-27 14:52:39 -0500

    code review

--- lms/envs/production.py
+++ lms/envs/production.py
@@ -28,11 +28,11 @@ from corsheaders.defaults import default_headers as corsheaders_default_headers
 from path import Path as path
 from xmodule.modulestore.modulestore_settings import convert_module_store_setting_if_needed
 from openedx.core.djangoapps.plugins import plugin_settings, constants as plugin_constants  # pylint: disable=wrong-import-order, wrong-import-position, ungrouped-imports
+from django.core.exceptions import ImproperlyConfigured
 
 from .common import *
 from openedx.core.lib.derived import derive_settings  # pylint: disable=wrong-import-order
 from openedx.core.lib.logsettings import get_logger_config  # pylint: disable=wrong-import-order
-from django.core.exceptions import ImproperlyConfigured  # pylint: disable=wrong-import-order
 
 
 def get_env_setting(setting):
